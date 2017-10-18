7650ea7e961d979cadb8159bcf8fef09ecb13532
Shubham Rekkawar <31854944+rekkawarShubhzz@users.noreply.github.com>
2017-10-18 11:21:43 +0530

    Update Assignment3.cpp

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : poly.cpp
-// Author      : 
+// Author      : Shubham Rekkawar
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
